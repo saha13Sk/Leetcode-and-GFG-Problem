3bc3c66b7942f298419fdd9a8fdfe8b9b1d95db3
Sourav Saha <102205180+souravsaha321@users.noreply.github.com>
2023-06-01 12:42:56 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (78.70%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        string res;
+    while (columnNumber>0)
+    {
+        char ch = 'A' + (columnNumber - 1) % 26;
+        res = ch + res ;
+        columnNumber = (columnNumber - 1) / 26;
+    }
+    return res;
+    }
+};

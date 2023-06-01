b901a33614455fe505a8cca8599c86ec248a8856
Sourav Saha <102205180+souravsaha321@users.noreply.github.com>
2023-06-01 13:34:06 +0530

    Time: 4 ms (37.84%), Space: 5.9 MB (92.90%) - LeetHub

---
+++ 2652-sum-multiples/2652-sum-multiples.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int sumOfMultiples(int n) {
+   long long int sum=0;
+       
+        for(long long int i =1; i<=n;i++){
+            
+           
+            if(i%3==0 || i%5==0 || i%7==0)
+            {
+                sum+=i;
+             }
+            
+        }
+        return sum;
+      //  return (int)sum/2 +1;
+      
+        
+    }
+};

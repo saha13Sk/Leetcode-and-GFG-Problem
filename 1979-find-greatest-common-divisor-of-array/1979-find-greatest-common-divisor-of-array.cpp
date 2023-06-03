3e16b74e3d3f7261a0c502b4c7401229201c20d3
Sourav Saha <102205180+souravsaha321@users.noreply.github.com>
2023-06-03 12:18:32 +0530

    Time: 0 ms (100.00%), Space: 12.5 MB (56.30%) - LeetHub

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.cpp
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.cpp
@@ -8,23 +8,9 @@ int a = *min_element(nums.begin(),nums.end());
 // max element of array
 int b = *max_element(nums.begin(),nums.end());
         return gcd(a,b);
-   /*    
-//  gcd(a,b) = gcd(a%b,b);
 
-while (a>0,b>0)
-{
-
-    if (a > b)
-        a = a % b;
-    else
-        b = b % a;
-}
-if (a == 0)
-    return b;
-else
-    return a;
         
-       */ 
+ 
         
     }
 };

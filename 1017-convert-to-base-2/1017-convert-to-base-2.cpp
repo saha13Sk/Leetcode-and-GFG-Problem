5db1272ef5453bda273e5fd4e2d26428ae603ce1
Sourav Saha <102205180+souravsaha321@users.noreply.github.com>
2023-06-06 12:21:17 +0530

    Time: 4 ms (37.24%), Space: 5.9 MB (85.71%) - LeetHub

---
+++ 1017-convert-to-base-2/1017-convert-to-base-2.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string baseNeg2(int n) {
+        
+        // note : divisible -2, n= n/-2
+       string str;
+        if(n==0) return "0";
+        while(n!=0){
+            int temp = n%(-2);
+            n/=(-2);
+            if(temp<0){
+                temp = temp + 2;   // convert positive number 
+                n++;                // actual number convert 2
+            }
+            str.insert(str.begin(),temp+'0');
+        }
+        return str;
+ 
+        
+    }
+};

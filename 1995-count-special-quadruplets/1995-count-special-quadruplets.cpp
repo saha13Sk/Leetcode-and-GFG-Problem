6918a2d07435ce74737d123c3b85e9435afc34e1
Sourav Saha <102205180+souravsaha321@users.noreply.github.com>
2023-06-14 10:40:15 +0530

    Time: 157 ms (48.17%), Space: 10.4 MB (69.08%) - LeetHub

--- 1995-count-special-quadruplets/1995-count-special-quadruplets.cpp
+++ 1995-count-special-quadruplets/1995-count-special-quadruplets.cpp
@@ -4,8 +4,7 @@ public:
         
         int cnt=0;
         int n = nums.size();
-       // sort(nums.begin(),nums.end());
-        
+      
         for(int i=0;i<n;i++){
             for(int j= i+1;j<n;j++){
                 for(int k=j+1;k<n;k++){

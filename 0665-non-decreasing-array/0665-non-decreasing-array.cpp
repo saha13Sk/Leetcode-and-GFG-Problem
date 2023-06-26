5ed57f506419c54d9157911471336daf321b282f
Sourav Saha <102205180+souravsaha321@users.noreply.github.com>
2023-06-26 17:04:05 +0530

    Time: 37 ms (24.33%), Space: 26.9 MB (76.58%) - LeetHub

---
+++ 0665-non-decreasing-array/0665-non-decreasing-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool checkPossibility(vector<int>& nums) {
+        
+          int cnt = 0;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i-1]>nums[i]){
+                cnt++;
+                if(i>=2&&nums[i-2]>nums[i]){
+                    nums[i]=nums[i-1];
+                }
+                else{
+                    nums[i-1]=nums[i];
+                }
+            }
+        }
+        return cnt<=1;
+    }
+};
